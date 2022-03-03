51e9456db3190ccac46f4efc9edc26531b4d2e4f
Magomed200121 <89939948+Magomed200121@users.noreply.github.com>
2022-03-03 18:05:54 +0300

    create

--- js/app.js
+++ js/app.js
@@ -5,3 +5,29 @@ const swiper = new Swiper('.swiper', {
         prevEl: '.swiper-button-prev',
     },
 });
+
+// Variables
+const menuItems = document.querySelectorAll('.header__menu-item')
+const burger = document.querySelector('.dialog-burger')
+const headerMenu = document.querySelector('.header__menu')
+
+// Event Listeners
+burger.addEventListener('click', showMenu)
+
+// Functions
+function showMenu(){
+    menuItems.forEach(item => {
+        item.style.display = 'block'
+        item.style.textAlign = 'center'
+    })
+    headerMenu.classList.toggle('show_menu')
+}
+
+MicroModal.init({
+    openTrigger: 'data-custom-open',
+    closeTrigger: 'data-custom-close',
+    disableFocus: true,
+    disableScroll: true,
+    awaitOpenAnimation: true,
+    awaitCloseAnimation: true
+})
